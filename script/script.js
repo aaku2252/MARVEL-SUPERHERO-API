e4884c928647295b4bc1e17974e486a94340e996
Akash <akkyjat601@gmail.com>
2023-10-15 19:19:18 +0530

    api setup

--- script/script.js
+++ script/script.js
@@ -1,1 +1,15 @@
+// console.log("hello world");
+const MD5 = require("crypto-js/md5");
 
+const privateKey = "";
+const publicKey = "";
+let ts = new Date().getTime();
+let hash = MD5(ts + privateKey + publicKey);
+
+let api = `https://gateway.marvel.com:443/v1/public/characters?ts=${ts}&apikey=${publicKey}&hash=${MD5(
+    ts + privateKey + publicKey
+)}`;
+
+fetch(api)
+    .then((response) => response.json())
+    .then((data) => console.log(typeof data.data.results[0].name));
