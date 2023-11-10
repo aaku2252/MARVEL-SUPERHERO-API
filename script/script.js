9e3cb7810155d0c3e3647ced96901a97cfe3a99b
Akash <akkyjat601@gmail.com>
2023-11-10 12:53:25 +0530

    marvel api project completed

--- script/script.js
+++ script/script.js
@@ -1,137 +1,128 @@
-// const MD5 = require("crypto-js/md5");
-// // need fuzzyset library
+const privateKey = "";
+const publicKey = "";
+let ts = new Date().getTime();
 
-// const privateKey = "";
-// const publicKey = "";
-// let ts = new Date().getTime();
-// let hash = MD5(ts + privateKey + publicKey);
+function generateMD5Hash(data) {
+    const hash = CryptoJS.MD5(data);
+    const hashHex = hash.toString(CryptoJS.enc.Hex);
+    return hashHex;
+}
+const inputString = ts + privateKey + publicKey;
+const md5Hash = generateMD5Hash(inputString);
 
-// let api = `https://gateway.marvel.com:443/v1/public/characters?ts=${ts}&apikey=${publicKey}&hash=${MD5(
-//     ts + privateKey + publicKey
-// )}`;
-// let filteredData;
-// fetch(api)
-//     .then((response) => response.json())
-//     .then((data) => filters(data));
+const home = document.querySelector("#home");
+const search_btn = document.querySelector("#search-btn");
+const search_bar = document.querySelector("#search-bar");
+const search_bar_btn = document.querySelector("#search-bar-btn");
+const home_cardbox = document.querySelector("#home_cardbox");
+const home_name_1 = document.querySelector("#home_name-1");
+const home_des_1 = document.querySelector("#home_des-1");
+const home_name_2 = document.querySelector("#home_name-2");
+const home_des_2 = document.querySelector("#home_des-2");
+const home_name_3 = document.querySelector("#home_name-3");
+const home_des_3 = document.querySelector("#home_des-3");
+const card = document.querySelectorAll(".card");
 
-// function filters(data) {
-//     filteredData = data.data.results;
-//     console.log(filteredData);
-// }
+const superheros = document.querySelector("#superheros");
+const btn = document.querySelector("#button");
+const superheros_name = document.querySelector("#superheros_name");
+const superheros_description = document.querySelector(
+    "#superheros_description"
+);
+const superheros_series = document.querySelector("#superheros_series");
+const superheros_comics = document.querySelector("#superheros_comics");
 
-// setTimeout(() => {
-//     console.log(filteredData);
-// }, 5000);
+let api = `https://gateway.marvel.com:443/v1/public/characters?ts=${ts}&apikey=${publicKey}&hash=${md5Hash}`;
+let filteredData;
+fetch(api)
+    .then((response) => response.json())
+    .then((data) => {
+        filteredData = data.data.results;
+        home_name_1.textContent = filteredData[1].name;
+        home_des_1.textContent = filteredData[1].description;
+        home_name_2.textContent = filteredData[2].name;
+        home_des_2.textContent = filteredData[2].description;
+        home_name_3.textContent = filteredData[15].name;
+        home_des_3.textContent = filteredData[15].description;
+        home_cardbox.style.visibility = "visible";
+    });
 
-// // Create a list of options.
-// const options = ["apple", "banana", "orange", "pear"];
+Array.from(card).map((x) =>
+    x.addEventListener("click", (e) => {
+        let name = e.currentTarget.querySelector("p").textContent;
+        let index = filteredData.findIndex((obj) => obj.name === name);
 
-// // Create a function to get suggestions for the given input text.
-// function getSuggestions(inputText) {
-//     const suggestions = [];
-//     for (const option of options) {
-//         if (difflib.SequenceMatcher(null, inputText, option).ratio() > 0.5) {
-//             suggestions.push(option);
-//         }
-//     }
-//     return suggestions;
-// }
+        superheros_name.textContent = filteredData[index].name;
+        superheros_description.textContent = filteredData[index].description;
+        let comics = "";
+        let series = "";
+        filteredData[index].comics.items.map((item) => {
+            comics += item.name + ", ";
+        });
+        filteredData[index].series.items.map((item) => {
+            series += item.name + ", ";
+        });
+        // console.log(series);
+        // console.log(superheros_series);
+        superheros_series.textContent = series;
+        superheros_comics.textContent = comics;
+        home.style.filter = "blur(20px)";
+        home.style.zIndex = -20;
+        superheros.style.visibility = "visible";
+        comics = null;
+        series = null;
+    })
+);
 
-// // Create a function to update the list of suggestions based on the given input text.
-// function updateSuggestions(inputText, suggestions) {
-//     const newSuggestions = [];
-//     for (const suggestion of suggestions) {
-//         if (inputText.includes(suggestion)) {
-//             newSuggestions.push(suggestion);
-//         }
-//     }
-//     return newSuggestions;
-// }
+search_btn.addEventListener("click", () => {
+    search_bar.focus();
+});
 
-// // Get the input element.
-// const input = document.querySelector("input");
+btn.addEventListener("click", () => {
+    home.style.filter = "none";
+    home.style.zIndex = 50;
+    superheros.style.visibility = "hidden";
+});
 
-// // Update the list of suggestions whenever the user types in the input bar.
-// input.addEventListener("input", () => {
-//     const inputText = input.value;
-//     const suggestions = getSuggestions(inputText);
+// console.log(stringSimilarity.compareTwoStrings("akash", "prakash"));
+// console.log(filteredData);
 
-//     // Display the suggestions to the user.
-//     // TODO: Implement this part.
-// });
+function matchingVal(val) {
+    let match = 0;
+    let index = 0;
+    filteredData.map((x) => {
+        let compareVal = stringSimilarity.compareTwoStrings(val, x.name);
+        if (compareVal > match) {
+            match = compareVal;
+            index = filteredData.findIndex((e) => e.name == x.name);
+        }
+    });
 
-// //>----------------------------------------------------------
-// function getSuggestions(inputText) {
-//     const suggestions = [];
-//     for (const option of options) {
-//         // Convert both inputText and option to lowercase for case-insensitive comparison
-//         const normalizedInput = inputText.toLowerCase();
-//         const normalizedOption = option.toLowerCase();
+    let comics = "";
+    let series = "";
+    filteredData[index].comics.items.map((item) => {
+        comics += item.name + ", ";
+    });
+    filteredData[index].series.items.map((item) => {
+        series += item.name + ", ";
+    });
 
-//         // Check for exact match or match with hyphen replaced by space
-//         if (
-//             normalizedInput === normalizedOption ||
-//             normalizedInput === normalizedOption.replace(/-/g, " ")
-//         ) {
-//             suggestions.push(option);
-//             continue;
-//         }
+    superheros_name.textContent = filteredData[index].name;
+    superheros_description.textContent = filteredData[index].description;
+    superheros_series.textContent = series;
+    superheros_comics.textContent = comics;
 
-//         // Check for match with variations in spacing
-//         const inputWords = normalizedInput.split(" ");
-//         const optionWords = normalizedOption.split(" ");
-//         if (inputWords.length === optionWords.length) {
-//             let matches = true;
-//             for (let i = 0; i < inputWords.length; i++) {
-//                 if (!optionWords.includes(inputWords[i])) {
-//                     matches = false;
-//                     break;
-//                 }
-//             }
-//             if (matches) {
-//                 suggestions.push(option);
-//             }
-//         }
-//     }
-//     return suggestions;
-// }
-// //>----------------------------------------------------------------
-// function getSuggestions(inputText) {
-//     const suggestions = [];
-//     for (const option of options) {
-//         // Calculate the similarity between the input text and the option, ignoring spaces and punctuation.
-//         const similarity = difflib
-//             .SequenceMatcher(
-//                 null,
-//                 inputText.replace(/\s+/g, "").replace(/[^\w]/g, ""),
-//                 option.replace(/\s+/g, "").replace(/[^\w]/g, "")
-//             )
-//             .ratio();
+    home.style.filter = "blur(20px)";
+    home.style.zIndex = -20;
+    superheros.style.visibility = "visible";
+    comics = null;
+    series = null;
+    match = null;
+    index = null;
+}
 
-//         // If the similarity is above a threshold, add the option to the suggestions list.
-//         if (similarity > 0.5) {
-//             suggestions.push(option);
-//         }
-//     }
-//     return suggestions;
-// }
-// //>----------------------------------------------------------------
-// function getSuggestions(inputText) {
-//     const suggestions = [];
-//     for (const option of options) {
-//         // Calculate the similarity between the input text and the option, ignoring spaces and punctuation.
-//         const similarity = difflib
-//             .SequenceMatcher(
-//                 null,
-//                 inputText.replace(/\s+/g, "").replace(/[^\w]/g, ""),
-//                 option.replace(/\s+/g, "").replace(/[^\w]/g, "")
-//             )
-//             .ratio();
-
-//         // If the similarity is above a threshold, add the option to the suggestions list.
-//         if (similarity > 0.5) {
-//             suggestions.push(option);
-//         }
-//     }
-//     return suggestions;
-// }
+search_bar_btn.addEventListener("click", () => {
+    if (filteredData.length > 0 && search_bar.value.length > 2) {
+        matchingVal(search_bar.value);
+    }
+});
