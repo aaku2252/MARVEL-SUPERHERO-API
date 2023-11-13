dec284c02d54940e6a5ce26dc74356a232dcc7d9
Akash <58019217+aaku2252@users.noreply.github.com>
2023-11-13 21:08:27 +0530

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
-const privateKey = "93df6da721fb7bb2cc48781c5545a5a04848b44e";
-const publicKey = "d7cb7d751f1fa9ea10240d6905c9062e";
+const privateKey = "";
+const publicKey = "";
 let ts = new Date().getTime();
 
 function generateMD5Hash(data) {
